e0355cf0e664b841c178694aeac3c029b947d9a0
surendra reddy <surendrareddylamuru01@gmail.com>
2023-02-23 14:06:45 +0530

    route table

--- subnets/lm-subnets/vars.tf
+++ subnets/lm-subnets/vars.tf
@@ -3,3 +3,4 @@ variable "vpc_id" {}
 variable "name" {}
 variable "cidr_block" {}
 variable "subnet_availability_zones" {}
+variable "route_table_id" {}
--- subnets/main.tf
+++ subnets/main.tf
@@ -6,9 +6,10 @@ module "lm-subnets" {
   name = each.value.name
   vpc_id = var.vpc_id[0]
   subnet_availability_zones = var.subnet_availability_zones
+  route_table_id = lookup(lookup(aws_route_table.aws_route-table, each.value.name, null), "id" null)
 }
 
-resource "aws_route_table" "route-table" {
+resource "aws_route_table" "aws_route-table" {
   for_each = var.subnets
   vpc_id    = var.vpc_id[0]
   tags = {
