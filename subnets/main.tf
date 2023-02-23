49c89e64f503b6307c68a2e4a9a5cfdf89757c18
surendra reddy <surendrareddylamuru01@gmail.com>
2023-02-23 12:55:52 +0530

    route table

--- subnets/main.tf
+++ subnets/main.tf
@@ -7,3 +7,13 @@ module "lm-subnets" {
   vpc_id = var.vpc_id[0]
   subnet_availability_zones = var.subnet_availability_zones
 }
+
+resource "aws_route_table" "route-table" {
+  for_each = var.subnets
+  vpc_id    = var.vpc_id[0]
+  tags = {
+    Name = "${var.env}-${each.value.name}-rt"
+    ENV= var.env
+    PROJECT = "roboshop"
+  }
+}
