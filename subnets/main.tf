98f1c50c1caa568017675dfd2b71f01719f04493
surendra reddy <surendrareddylamuru01@gmail.com>
2023-02-23 14:09:36 +0530

    route table

--- subnets/main.tf
+++ subnets/main.tf
@@ -6,7 +6,7 @@ module "lm-subnets" {
   name = each.value.name
   vpc_id = var.vpc_id[0]
   subnet_availability_zones = var.subnet_availability_zones
-  route_table_id = lookup(lookup(aws_route_table.aws_route-table, each.value.name, null), "id" null)
+  route_table_id = lookup(lookup(aws_route_table.aws_route-table, each.value.name, null), "id" , null)
 }
 
 resource "aws_route_table" "aws_route-table" {
