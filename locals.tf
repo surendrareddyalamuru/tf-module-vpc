1070bab860ab737a6a4f1a5bdff9cbab3ffa2691
surendra reddy <surendrareddylamuru01@gmail.com>
2023-02-23 12:22:16 +0530

    peering

--- locals.tf
+++ locals.tf
@@ -4,4 +4,10 @@ locals {
     ENV= var.env
     PROJECT = "roboshop"
   }
+  vpc_peering_tags = {
+    Name = "${var.env}-vpc-to-management-vpc"
+    ENV= var.env
+    PROJECT = "roboshop"
+  }
+  vpc_ids = [ for k, v in aws_vpc.main : v.id ]
 }
