5f3a5d0294630976d29cc2b05bbdd7659cb28873
surendra reddy <surendrareddylamuru01@gmail.com>
2023-03-01 13:04:36 +0530

    peering

--- subnets.tf
+++ subnets.tf
@@ -1,10 +1,23 @@
-module "subnets" {
+module "public_subnets" {
   for_each = var.vpc
-  subnets = each.value.subnets
+  subnets = each.value.public_subnets
   source = "./subnets"
   vpc_id = [ for k, v in aws_vpc.main : v.id ]
   env = var.env
   subnet_availability_zones = each.value.subnet_availability_zones
   peering_connection_id = aws_vpc_peering_connection.management-vpc-to-env-vpc.*.id[0]
   management_vpc = var.management_vpc
+  internet_gateway_id = aws_internet_gateway.igw.*.id[0]
+}
+
+module "private_subnets" {
+  for_each = var.vpc
+  subnets = each.value.private_subnets
+  source = "./subnets"
+  vpc_id = [ for k, v in aws_vpc.main : v.id ]
+  env = var.env
+  subnet_availability_zones = each.value.subnet_availability_zones
+  peering_connection_id = aws_vpc_peering_connection.management-vpc-to-env-vpc.*.id[0]
+  management_vpc = var.management_vpc
+  internet_gateway_id = aws_internet_gateway.igw.*.id[0]
 }
