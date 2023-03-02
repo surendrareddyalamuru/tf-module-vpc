ab25830ce9c9550ef1ccdee379e47d2ea8b1ea38
surendra reddy <surendrareddylamuru01@gmail.com>
2023-03-02 18:02:44 +0530

    peering

--- subnets.tf
+++ subnets.tf
@@ -7,7 +7,7 @@ module "public_subnets" {
   subnet_availability_zones = each.value.subnet_availability_zones
   peering_connection_id = aws_vpc_peering_connection.management-vpc-to-env-vpc.*.id[0]
   management_vpc = var.management_vpc
-  gateway_id                = aws_internet_gateway.gw.id
+  internet_gateway_id                = aws_internet_gateway.gw.id
 }
 
 module "private_subnets" {
@@ -19,5 +19,5 @@ module "private_subnets" {
   subnet_availability_zones = each.value.subnet_availability_zones
   peering_connection_id = aws_vpc_peering_connection.management-vpc-to-env-vpc.*.id[0]
   management_vpc = var.management_vpc
-  gateway_id                = aws_nat_gateway.ngw.id
+  internet_gateway_id                = aws_nat_gateway.ngw.id
 }
