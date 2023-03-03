332d7f78c39c4fb7191479e1d82f87989a477093
surendra reddy <surendrareddylamuru01@gmail.com>
2023-03-03 11:35:40 +0530

    peering

--- subnets.tf
+++ subnets.tf
@@ -1,5 +1,5 @@
 module "public_subnets" {
-  for_each = var.public_subnets
+  for_each = var.vpc
   subnets = each.value.public_subnets
   source = "./subnets"
   vpc_id = [ for k, v in aws_vpc.main : v.id ]
@@ -11,7 +11,7 @@ module "public_subnets" {
 }
 
 module "private_subnets" {
-  for_each = var.private_subnets
+  for_each = var.vpc
   subnets = each.value.private_subnets
   source = "./subnets"
   vpc_id = [ for k, v in aws_vpc.main : v.id ]
--- vars.tf
+++ vars.tf
@@ -2,5 +2,3 @@ variable "vpc" {}
 
 variable "env" {}
 variable "management_vpc" {}
-variable "public_subnets" {}
-variable "private_subnets" {}
