5dfdaa2efb6bdc61ac3e8b7ca937d05850134b6f
surendra reddy <surendrareddylamuru01@gmail.com>
2023-03-03 11:50:17 +0530

    peering

--- subnets/main.tf
+++ subnets/main.tf
@@ -9,27 +9,27 @@ module "lm-subnets" {
   route_table_id = lookup(lookup(aws_route_table.aws_route-table, each.value.name, null), "id" , null)
 }
 
-locals {
-  subnets_list = flatten([for i, j in module.lm-subnets : j.subnets])
-}
-
-output "subnetz-list" {
-  value = local.subnets_list[*].id
-}
-
-#resource "aws_route_table" "aws_route-table" {
-#  for_each = var.subnets
-#  vpc_id    = var.vpc_id[0]
-#  tags = {
-#    Name = "${var.env}-${each.value.name}-rt"
-#    ENV= var.env
-#    PROJECT = "roboshop"
-#  }
+#locals {
+#  subnets_list = flatten([for i, j in module.lm-subnets : j.subnets])
 #}
 #
-#resource "aws_route" "peering_connection_route" {
-#  for_each = var.subnets
-#  route_table_id            = lookup(lookup(aws_route_table.aws_route-table, each.value.name, null), "id" , null)
-#  destination_cidr_block    = lookup(var.management_vpc, "vpc_cidr", null)
-#  vpc_peering_connection_id = var.peering_connection_id
-#}
+#output "subnetz-list" {
+#  value = local.subnets_list[*].id
+}
+
+resource "aws_route_table" "aws_route-table" {
+  for_each = var.subnets
+  vpc_id    = var.vpc_id[0]
+  tags = {
+    Name = "${var.env}-${each.value.name}-rt"
+    ENV= var.env
+    PROJECT = "roboshop"
+  }
+}
+
+resource "aws_route" "peering_connection_route" {
+  for_each = var.subnets
+  route_table_id            = lookup(lookup(aws_route_table.aws_route-table, each.value.name, null), "id" , null)
+  destination_cidr_block    = lookup(var.management_vpc, "vpc_cidr", null)
+  vpc_peering_connection_id = var.peering_connection_id
+}
