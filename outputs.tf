20cd84587e4ab83dea96bdc66d9238ef4847f0e8
surendra reddy <surendrareddylamuru01@gmail.com>
2023-03-08 10:51:02 +0530

    outputs

--- outputs.tf
+++ outputs.tf
@@ -6,7 +6,3 @@ output "public_subnets" {
 output "private_subnets" {
   value = module.private_subnets
 }
-
-output "test" {
-  value = flatten([for i, j in module.private_subnets : j.subnets-list])
-}
