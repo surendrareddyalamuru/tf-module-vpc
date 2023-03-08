eb0091d06c19dcd7163e1d099c6c7977190428db
surendra reddy <surendrareddylamuru01@gmail.com>
2023-03-08 18:07:47 +0530

    outputs

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,7 @@
-output "public_subnets" {
-  value = module.public_subnets
-}
-
-
 output "private_subnets" {
   value = module.private_subnets
+}
+
+output "public_subnets" {
+  value = module.public_subnets
 }
