bc3f767aa2c18ce1d8f8fcdbb39363e958757a90
surendra reddy <surendrareddylamuru01@gmail.com>
2023-03-10 11:33:29 +0530

    subnets

--- subnets/outputs.tf
+++ subnets/outputs.tf
@@ -1,24 +1,24 @@
-output "subnets" {
-  value = module.lm-subnets
-}
-
-locals {
-  subnets_list = flatten([for i, j in module.lm-subnets : j.subnets])
-}
-
-output "subnets-list" {
-  value = local.subnets_list[*].id
-}
-
-
+#output "subnets" {
+#  value = module.lm-subnets
+#}
+#
+#locals {
+#  subnets_list = flatten([for i, j in module.lm-subnets : j.subnets])
+#}
+#
+#output "subnets-list" {
+#  value = local.subnets_list[*].id
+#}
 
 
-output "rt" {
-  value = [ for i, j in aws_route_table.aws_route-table : j.id ]
-  #{
-  #  for k, v in aws_route_table.aws_route-table : k => v.id
-  #  }
-}
+
+#
+#output "rt" {
+#  value = [ for i, j in aws_route_table.aws_route-table : j.id ]
+#  #{
+#  #  for k, v in aws_route_table.aws_route-table : k => v.id
+#  #  }
+#}
 
 #output "subnets" {
 #  value = flatten([for i, j in module.lm-subnets: j.subnets])
