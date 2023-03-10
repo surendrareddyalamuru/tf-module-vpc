a8ea0f33b039a4e2a55fc77bd5f68fe50abe5ef2
surendra reddy <surendrareddylamuru01@gmail.com>
2023-03-10 12:38:21 +0530

    subnets list

--- gw.tf
+++ gw.tf
@@ -10,15 +10,15 @@ resource "aws_eip" "ngw" {
   vpc      = true
 }
 
-#resource "aws_nat_gateway" "ngw" {
-#  allocation_id = aws_eip.ngw.id
-#  subnet_id     = local.public_subnets_list[0]
-#
-#  tags = {
-#    Name = "NAT Gw"
-#  }
-#}
-#
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw.id
+  subnet_id     = lookup(lookup(module.public_subnets, "public", null), "subnets", null)[0].id
+
+  tags = {
+    Name = "NAT Gw"
+  }
+}
+
 #locals {
 #  private_route_tables = flatten([for i, j in module.private_subnets : j.rt])
 #  private_subnets_list = flatten([for i, j in module.private_subnets : j.subnets-list])
