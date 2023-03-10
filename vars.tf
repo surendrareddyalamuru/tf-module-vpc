8332347355557a9f6b3cc6b9bfa0ea9ca05d1cd9
surendra reddy <surendrareddylamuru01@gmail.com>
2023-03-10 11:04:33 +0530

    vpc

--- vars.tf
+++ vars.tf
@@ -3,3 +3,5 @@ variable "cidr_block" {}
 variable "additional_cidr_block" {}
 variable "private_subnets" {}
 variable "public_subnets" {}
+variable "subnet_availability_zones" {}
+variable "management_vpc" {}
