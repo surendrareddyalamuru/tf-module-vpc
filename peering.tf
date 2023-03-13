220a8ae731c36714e12cdad8c7b19b87db9b1096
surendra reddy <surendrareddylamuru01@gmail.com>
2023-03-13 10:44:19 +0530

    route tables

--- peering.tf
+++ peering.tf
@@ -5,9 +5,9 @@ resource "aws_vpc_peering_connection" "management-vpc-to-env-vpc" {
   tags = local.vpc_peering_tags
 }
 
-#resource "aws_route" "route-to-default-management-route-table" {
-#  for_each = var.vpc
-#  route_table_id            = lookup(var.management_vpc, "route_table", null)
-#  destination_cidr_block    = each.value.cidr_block
-#  vpc_peering_connection_id = aws_vpc_peering_connection.management-vpc-to-env-vpc.*.id[0]
-#}
+resource "aws_route" "route-to-default-management-route-table" {
+  count = length(local.all_route_tables)
+  route_table_id            = element(local.all_route_tables, count.index)
+  destination_cidr_block    = lookup(var.management_vpc, "vpc_cidr", null)
+  vpc_peering_connection_id = aws_vpc_peering_connection.management-vpc-to-env-vpc.id
+}
