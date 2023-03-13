a114b351395d143210b6c5fa01f76bda3a194c93
surendra reddy <surendrareddylamuru01@gmail.com>
2023-03-13 11:39:49 +0530

    docdb

--- subnets/lm-subnets/locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  subnet_tags = {
-    Name = "${var.name}-subnet"
-    ENV= var.env
-    PROJECT = "roboshop"
-  }
-}
--- subnets/lm-subnets/vars.tf
+++
@@ -1,6 +0,0 @@
-variable "env" {}
-variable "vpc_id" {}
-variable "name" {}
-variable "cidr_block" {}
-variable "subnet_availability_zones" {}
-variable "route_table_id" {}
